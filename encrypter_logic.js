128bd5d060bd4abc9d47e15b4ad1e6bcba17ee37
Daniel Mass Dominguez <danielmassdominguez@gmail.com>
2023-06-30 02:19:53 -0600

    encrypt,decrypt and copy function are added

--- encrypter_logic.js
+++ encrypter_logic.js
@@ -8,11 +8,37 @@ function showTextElements(){
     noTextElements.style.display = "none";
 }
 
+function encrypt(str) {
+    const keys = {
+        a: 'ai',
+        e: 'enter',
+        i: 'imes',
+        o: 'ober',
+        u: 'ufat'
+    };
+  
+    return str.replace(/[aeiou]/g, match => keys[match.toLowerCase()]);
+}
+
+function decrypt(str) {
+    const keys = {
+        ai:     'a',
+        enter:  'e',
+        imes:   'i',
+        ober:   'o',
+        ufat:   'u'
+    };
+  
+    return str.replace(/ai|enter|imes|ober|ufat/g, match => keys[match.toLowerCase()]);
+}
+
 let encryptButton = document.querySelector(".encrypt-button");
 let decryptButton = document.querySelector(".decrypt-button");
+let copyTextButton = document.querySelector(".copyTextButton");
 let textElements = document.querySelector(".outputTextPart");
 let noTextElements = document.querySelector(".noTextPart");
 let textInput = document.querySelector(".textInput");
+let resultText = document.querySelector(".resultText");
 
 textInput.addEventListener("input", function() {
     if (textInput.value === "") {
@@ -20,5 +46,18 @@ textInput.addEventListener("input", function() {
     }
   });
 
-encryptButton.addEventListener("click", showTextElements);
-decryptButton.addEventListener("click", showTextElements);
+encryptButton.addEventListener("click", function() {
+    showTextElements();
+    resultText.value = encrypt(textInput.value);
+});
+
+decryptButton.addEventListener("click", function() {
+    showTextElements();
+    resultText.value = decrypt(textInput.value);
+});
+
+copyTextButton.addEventListener("click", function() {
+    resultText.select();
+    document.execCommand("copy");
+    resultText.blur();
+});
