a1ffaecbe10b51c9e8bba79572445a24793ef3a4
Daniel Mass Dominguez <danielmassdominguez@gmail.com>
2023-06-29 21:04:10 -0600

    JS file added, parity with HTML file OK

---
+++ encrypter_logic.js
@@ -0,0 +1,8 @@
+function prueba(){
+    encryptButton.style.backgroundColor = "green";
+}
+
+let encryptButton = document.querySelector(".encrypt-button");
+let decryptButton = document.querySelector(".decrypt-button");
+
+encryptButton.addEventListener("click", prueba);
