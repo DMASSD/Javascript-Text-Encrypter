f12b9e74822cd3b78bfd63375bdcd5e4db3a5f1e
Daniel Mass Dominguez <danielmassdominguez@gmail.com>
2023-06-30 01:13:11 -0600

    No text part and text output are now hideble

--- encrypter_logic.js
+++ encrypter_logic.js
@@ -1,21 +1,24 @@
-// function hideNoTextElements(){
-//     noTextElements.style.display = "none";
-// }
+function hideTextElements(){
+    textElements.style.display = "none";
+    noTextElements.style.display = "block";
+}
 
-// function showNoTextElements(){
-//     noTextElements.style.display = "block";
-// }
+function showTextElements(){
+    textElements.style.display = "block";
+    noTextElements.style.display = "none";
+}
 
-// let encryptButton = document.querySelector(".encrypt-button");
-// let decryptButton = document.querySelector(".decrypt-button");
-// let noTextElements = document.querySelector(".noTextPart");
-// let textInput = document.querySelector(".textInput");
+let encryptButton = document.querySelector(".encrypt-button");
+let decryptButton = document.querySelector(".decrypt-button");
+let textElements = document.querySelector(".outputTextPart");
+let noTextElements = document.querySelector(".noTextPart");
+let textInput = document.querySelector(".textInput");
 
-// textInput.addEventListener("input", function() {
-//     if (textInput.value === "") {
-//       showNoTextElements();
-//     }
-//   });
+textInput.addEventListener("input", function() {
+    if (textInput.value === "") {
+        hideTextElements();
+    }
+  });
 
-// encryptButton.addEventListener("click", hideNoTextElements);
-// decryptButton.addEventListener("click", hideNoTextElements);
+encryptButton.addEventListener("click", showTextElements);
+decryptButton.addEventListener("click", showTextElements);
